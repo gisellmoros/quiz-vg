1c390729d585cc24d81e40e38d58cabb8bf9406e
gisellmoros <morosgisell@gmail.com>
2021-06-27 20:41:11 +0400

    Installed material ui, first clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {QuizProvider} from 'context/quiz';
 
 function App() {
+
+  const quizProviderValues = {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QuizProvider value={quizProviderValues}>
+      App
+    </QuizProvider>
   );
 }
 
